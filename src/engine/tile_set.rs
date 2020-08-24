cb3f745129be67b473d575bf2c72884824974b4d
mcb2003 <mikeybuchan@hotmail.co.uk>
2020-08-25 00:00:39 +0100

    Only calculate TileSet dimentions on construction, not every render.

--- src/engine/tile_set.rs
+++ src/engine/tile_set.rs
@@ -4,6 +4,8 @@ use super::{SolidBlock, Tile, TILE_SIZE};
 
 pub struct TileSet {
     tiles: Vec<Option<Box<dyn Tile>>>,
+    width: i32,
+    height: i32,
 }
 
 impl TileSet {
@@ -16,17 +18,20 @@ impl TileSet {
                 None
             });
         }
-        Self { tiles }
+        Self {
+            tiles,
+            width: olc::screen_width() / TILE_SIZE,
+            height: olc::screen_height() / TILE_SIZE,
+        }
     }
 
     pub fn draw(&self) {
-        let width = olc::screen_width() / TILE_SIZE;
-        let height = olc::screen_height() / TILE_SIZE;
         olc::clear(olc::BLACK);
         let mut pos = Vi2d { x: 0, y: 0 };
-        while pos.y < height {
-            while pos.x < width {
-                if let Some(tile) = &self.tiles[(pos.y * width + pos.x) as usize] {
+        while pos.y < self.height {
+            while pos.x < self.width {
+                let idx = (pos.y * self.width + pos.x) as usize;
+                if let Some(tile) = &self.tiles[idx] {
                     tile.draw(pos);
                 }
                 pos.x += 1;

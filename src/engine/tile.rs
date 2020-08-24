e47ddede75f15df4568fdd33f712596d286a5896
mcb2003 <mikeybuchan@hotmail.co.uk>
2020-08-24 22:24:35 +0100

    Simplify drawing code for implementors of Tile trait.
    The caller calls Tile::draw() with the tile's position.
    * The callee implements Tile::draw_at(), which recieves the position in screen space.

--- src/engine/tile.rs
+++ src/engine/tile.rs
@@ -4,5 +4,10 @@ use olc_pixel_game_engine::Vi2d;
 use super::TILE_SIZE;
 
 pub trait Tile {
-    fn draw(&self, pos: Vi2d);
+    fn draw_at(&self, pos: Vi2d);
+    fn draw(&self, mut pos: Vi2d) {
+        pos.x *= TILE_SIZE;
+        pos.y *= TILE_SIZE;
+        self.draw_at(pos);
+    }
 }

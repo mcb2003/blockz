4bc2f12fc1ec6bf19afe7826121fbd7cd962b5ee
mcb2003 <mikeybuchan@hotmail.co.uk>
2020-08-29 21:36:51 +0100

    Add OLC[3] License and produce Pixel Game Engine copyright notice on startup.

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,9 @@ use olc_pixel_game_engine as olc;
 use blockz::engine::{Engine, APP_NAME, APP_VERSION};
 
 fn main() -> Result<(), olc::Error> {
+    println!("{}, version {}.", APP_NAME, APP_VERSION);
+    println!("Made with the One Lone Coder Pixel Game Engine: Copyright 2018, 2019, 2020 OneLoneCoder.com.");
+    println!("https://github.com/OneLoneCoder/olcPixelGameEngine");
     let mut app = Engine::new();
     let title = format!("{} v{}", APP_NAME, APP_VERSION);
     match olc::start(title.as_str(), &mut app, 256, 240, 4, 4) {
